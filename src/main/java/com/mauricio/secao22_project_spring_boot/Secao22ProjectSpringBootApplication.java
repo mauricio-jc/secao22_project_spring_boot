750ca213530c869a5daade0be730224a413a8704
Maurício Citadini Biléssimo <mauricioc.bilessimo@gamil.com>
2025-05-20 17:16:06 -0300

    Commit inicial

---
+++ src/main/java/com/mauricio/secao22_project_spring_boot/Secao22ProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mauricio.secao22_project_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Secao22ProjectSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Secao22ProjectSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauricio/secao22_project_spring_boot/Secao22ProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauricio.secao22_project_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Secao22ProjectSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
